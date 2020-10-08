67cd52e538fc97074a0e4ce53707aa801180249d
SorehaDAME <51748896+ag0609@users.noreply.github.com>
2020-10-08 18:31:50 +0800

    Update dlsitejp.inc.js

--- BNDL_UserScript/Current/Plugin/dlsitejp.inc.js
+++ BNDL_UserScript/Current/Plugin/dlsitejp.inc.js
@@ -19,7 +19,7 @@ function loadcache(startidx=0, path=tp) {
     } catch(e) {
         console.warn("loadcache()", "path", path, "invaild, using current tree path.");
         if(tp && tp != "") {
-            cpobj = /\.pdf/i.test(tp) ? (/\//.test(tp) ? searchPath(zt.tree, tp, "name").children : zt.tree) : searchinJSON(zt.tree, tp, "path")[0].children;
+            cpobj = /\.pdf/i.test(tp) ? (/\//.test(tp) ? searchinJSON(zt.tree, searchPath(zt.tree, tp.replace(/^.*\/([^\/]+\.[^\/]+)$/, "$1"), "name"), "name")[0].children : zt.tree) : searchinJSON(zt.tree, tp, "path")[0].children;
             if(cache[tp] == undefined) cache[tp] = {"used":0};
             cp = cache[tp];
         } else {
