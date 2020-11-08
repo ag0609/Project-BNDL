371c2399a4759338bc12f20f42a761e395e3e712
SorehaDAME <51748896+ag0609@users.noreply.github.com>
2020-11-08 11:42:45 +0900

    Update BNDL_P.user.js
    + ptrial for free 10 mins novel reading scheme
    + add DL ComicInfo.xml for debug

--- BNDL_UserScript/Current/BNDL_P.user.js
+++ BNDL_UserScript/Current/BNDL_P.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BNDL collector(Plugin version)
 // @namespace    https://github.com/ag0609/Project-BNDL
-// @version      0.2
+// @version      0.3
 // @description  Don't use if you don't know what is this
 // @author       ag0609
 // @match        https://*.bookwalker.jp/*
@@ -193,6 +193,20 @@
         bndl_d.comicInfo = () => {
             console.group("comicInfo: show comicInfo.xml");
             console.debug(Ci);
+            if(arguments[0]) {
+                let serializer = new XMLSerializer();
+                let xmlStr = '<?xml version="1.0"?>\n' + serializer.serializeToString(xml);
+                let blob = new Blob([xmlStr], {type: "text/xml"});
+                const Url = window.URL.createObjectURL(blob);
+                const e = new MouseEvent("click");
+                const a = document.createElement('a');
+                a.innerHTML = 'Download';
+                a.download = "ComicInfo.xml";
+                a.href = Url;
+                a.dispatchEvent(e);
+                URL.revokeObjectURL(blob);
+                console.debug("Done");
+            }
             console.groupEnd();
         }
         bndl_d.next = () => { console.warn("no function yet"); }
@@ -257,7 +271,7 @@
     const halfwidthValue = (value) => {return value.replace(/[\uff01-\uff5e]/g, fullwidthChar => String.fromCharCode(fullwidthChar.charCodeAt(0) - 0xfee0)).replace(/\u3000/g, '\u0020')}
     let jsMain = "";
     let start = ()=>{}, cancel = ()=>{};
-    if(/viewer(?:\-trial)?\.bookwalker\.jp/i.test(window.location.href)) jsMain = GM_getResourceText("BWJP");
+    if(/viewer(?:\-p?trial)?\.bookwalker\.jp/i.test(window.location.href)) jsMain = GM_getResourceText("BWJP");
     //if(/bookwalker\.tw/i.test(window.location.href)) jsMain = GM_getResourceText("BWTW");
     if(/booklive\.jp/i.test(window.location.href)) jsMain = GM_getResourceText("BLJP");
     if(/play\.dlsite\.com/i.test(window.location.href)) jsMain = GM_getResourceText("DLJP");
