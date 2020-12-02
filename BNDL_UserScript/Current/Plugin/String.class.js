aa556fa85385bc721004156c0ee85fd57db3a970
SorehaDAME <51748896+ag0609@users.noreply.github.com>
2020-12-02 18:12:21 +0800

    Create String.class.js

---
+++ BNDL_UserScript/Current/Plugin/String.class.js
@@ -0,0 +1,38 @@
+let ver_similarity = "20201201.1";
+
+//
+console.log("String.similarity", ver_similarity, "loaded");
+
+Object.defineProperty(String.prototype, "similarity", {value:function(key, options={}) {
+    let [a,b] = [key, options];
+    let c = arguments[2];
+    if(!c) c = this;
+    let score = 0;
+    let matchScore = parseFloat(b.MatchScore) || 50, flowScore = parseFloat(b.FlowScore) || 50;
+    let score1=0, score2=0;
+    //keys match
+    if(typeof key == "string") a = new RegExp(a, "");
+    console.log(matchScore, flowScore);
+    if(b.AllMatch) {
+        score1 = a.test(c) ? matchScore : 0;
+    } else {
+        if(a.source.length*2 > c.length) {
+            for(let i=a.source.length; i>0; i--) {
+                if((new RegExp(a.source.substr(0, i))).test(c)) {
+                    score1 = i * matchScore/a.source.length;
+                    break;
+                }
+            }
+        } else {
+            for(let i=1; i<=a.source.length; i++) {
+                if((new RegExp(a.source.substr(0, i))).test(c)) {
+                    score1 = i * matchScore/a.source.length;
+                }
+            }
+        }
+    }
+    if(score1) {
+        score2 = flowScore - (flowScore * Math.abs(c.length - a.source.length)/Math.max(c.length, a.source.length));
+    }
+    return {"score":score1+score2, "totalScore":matchScore+flowScore, "source":c, "key":a.source, "MatchScore":score1,"maxMatchScore":matchScore, "FlowScore":score2, "maxFlowScore":flowScore};
+}});
