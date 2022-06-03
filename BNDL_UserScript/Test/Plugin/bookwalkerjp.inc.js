e4bf6d6bbb6d19282e23a2cf168cb0093cf2258f
SorehaDAME <51748896+ag0609@users.noreply.github.com>
2022-06-03 16:32:44 +0900

    Update bookwalkerjp.inc.js

--- BNDL_UserScript/Test/Plugin/bookwalkerjp.inc.js
+++ BNDL_UserScript/Test/Plugin/bookwalkerjp.inc.js
@@ -1,5 +1,5 @@
 //Reference Discramer
-console.log("BW Japan", "v20220603.0");
+console.log("BW Japan", "v20220603.1");
 console.log("Reference:", "https://fireattack.wordpress.com/", "by fireattack");
 let _detail$retry_ = 0;
 let backup, control, menu, renderer, model;
